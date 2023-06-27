8c06825f2f6382ae5f36f9ce64141a565c0f2c30
Kseniia Kovbasiuk <kseniia.kovbasiuk@valtech.com>
2023-06-27 19:43:23 +0300

    init commit

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const logger = require("morgan");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT;
+const DB_HOST = process.env.DB_HOST;
 
 const bookRouter = require("./src/routers/router");
 
@@ -23,6 +25,25 @@ message: "Not found",
 });
 });
 
+// app.use((error, _, res, _) => {
+// res.status(500).json({
+// message: error.message,
+// });
+// });
+
+mongoose
+.connect(DB_HOST)
+.then(() => {
+console.log("DB Connected!");
 app.listen(PORT, () => {
 console.log(`Server is running on port ${PORT}`);
 });
+})
+.catch((error) => {
+console.error(error);
+process.exit(1);
+});
+
+// app.listen(PORT, () => {
+// console.log(`Server is running on port ${PORT}`);
+// });
